5c279369598f64ae580a1dd8936ae2c7a498aa07
Diego <diego.pazos@datadope.io>
2019-09-09 17:18:00 +0200

    Add compatibility with Autoregister mode Zabbix Function sends a tcp packet with a unique host metadata id to add automatically the host in Zabbix

--- zabbix.go
+++ zabbix.go
@@ -1,28 +1,41 @@
-// Package implement zabbix sender protocol for send metrics to zabbix.
+// Package zabbix implements the sender protocol to send values to zabbix
+// Taken from github.com/blacked/go-zabbix (discontinued)
 package zabbix
 
 import (
+	"bytes"
 	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net"
+	"strconv"
 	"time"
+)
+
+const (
+	defaultConnectTimeout = 5 * time.Second
+	defaultWriteTimeout   = 5 * time.Second
+	// A heavy loaded Zabbix server processing several metrics,
+	// containing LLDs, could take several seconds to respond
+	defaultReadTimeout = 15 * time.Second
 )
 
 // Metric class.
 type Metric struct {
-	Host  string `json:"host"`
-	Key   string `json:"key"`
-	Value string `json:"value"`
-	Clock int64  `json:"clock"`
+	Host   string `json:"host"`
+	Key    string `json:"key"`
+	Value  string `json:"value"`
+	Clock  int64  `json:"clock,omitempty"`
+	Active bool   `json:"-"`
 }
 
-// Metric class constructor.
-func NewMetric(host, key, value string, clock ...int64) *Metric {
-	m := &Metric{Host: host, Key: key, Value: value}
-	// use current time, if `clock` is not specified
-	if m.Clock = time.Now().Unix(); len(clock) > 0 {
+// NewMetric return a zabbix Metric with the values specified
+// agentActive should be set to true if we are sending to a Zabbix Agent (active) item
+func NewMetric(host, key, value string, agentActive bool, clock ...int64) *Metric {
+	m := &Metric{Host: host, Key: key, Value: value, Active: agentActive}
+	// do not send clock if not defined
+	if len(clock) > 0 {
 		m.Clock = int64(clock[0])
 	}
 	return m
@@ -30,16 +43,32 @@ func NewMetric(host, key, value string, clock ...int64) *Metric {
 
 // Packet class.
 type Packet struct {
-	Request string    `json:"request"`
-	Data    []*Metric `json:"data"`
-	Clock   int64     `json:"clock"`
+	Request      string    `json:"request"`
+	Data         []*Metric `json:"data,omitempty"`
+	Clock        int64     `json:"clock,omitempty"`
+	Host         string    `json:"host,omitempty"`
+	HostMetadata string    `json:"host_metadata,omitempty"`
 }
 
-// Packet class cunstructor.
-func NewPacket(data []*Metric, clock ...int64) *Packet {
-	p := &Packet{Request: `sender data`, Data: data}
-	// use current time, if `clock` is not specified
-	if p.Clock = time.Now().Unix(); len(clock) > 0 {
+// Reponse is a response for autoregister method
+type Response struct {
+	Response string
+	Info     string
+}
+
+// NewPacket return a zabbix packet with a list of metrics
+func NewPacket(data []*Metric, agentActive bool, clock ...int64) *Packet {
+	var request string
+	if agentActive {
+		request = "agent data"
+	} else {
+		request = "sender data"
+	}
+
+	p := &Packet{Request: request, Data: data}
+
+	// do not send clock if not defined
+	if len(clock) > 0 {
 		p.Clock = int64(clock[0])
 	}
 	return p
@@ -55,116 +84,174 @@ func (p *Packet) DataLen() []byte {
 
 // Sender class.
 type Sender struct {
-	Host string
-	Port int
+	Host           string
+	Port           int
+	ConnectTimeout time.Duration
+	ReadTimeout    time.Duration
+	WriteTimeout   time.Duration
 }
 
-// Sender class constructor.
+// NewSender return a sender object to send metrics using default values for timeouts
 func NewSender(host string, port int) *Sender {
-	s := &Sender{Host: host, Port: port}
-	return s
+	return &Sender{
+		Host:           host,
+		Port:           port,
+		ConnectTimeout: defaultConnectTimeout,
+		ReadTimeout:    defaultReadTimeout,
+		WriteTimeout:   defaultWriteTimeout,
+	}
 }
 
-// Method Sender class, return zabbix header.
+// NewSenderTimeout return a sender object to send metrics defining values for timeouts
+func NewSenderTimeout(
+	host string,
+	port int,
+	connectTimeout time.Duration,
+	readTimeout time.Duration,
+	writeTimeout time.Duration,
+) *Sender {
+	return &Sender{
+		Host:           host,
+		Port:           port,
+		ConnectTimeout: connectTimeout,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+	}
+}
+
+// getHeader return zabbix header.
+// https://www.zabbix.com/documentation/4.0/manual/appendix/protocols/header_datalen
 func (s *Sender) getHeader() []byte {
 	return []byte("ZBXD\x01")
 }
 
-// Method Sender class, resolve uri by name:port.
-func (s *Sender) getTCPAddr() (iaddr *net.TCPAddr, err error) {
-	// format: hostname:port
-	addr := fmt.Sprintf("%s:%d", s.Host, s.Port)
-
-	// Resolve hostname:port to ip:port
-	iaddr, err = net.ResolveTCPAddr("tcp", addr)
+// read data from connection.
+func (s *Sender) read(conn net.Conn) ([]byte, error) {
+	res, err := ioutil.ReadAll(conn)
 	if err != nil {
-		err = fmt.Errorf("Connection failed: %s", err.Error())
-		return
+		return res, fmt.Errorf("receiving data: %s", err.Error())
 	}
 
-	return
+	return res, nil
 }
 
-// Method Sender class, make connection to uri.
-func (s *Sender) connect() (conn *net.TCPConn, err error) {
+// SendMetrics send an array of metrics, making different packets for
+// trapper and active items.
+// The response for trapper metrics is in the first element of the res array and err array
+// Response for active metrics is in the second element of the res array and error array
+func (s *Sender) SendMetrics(metrics []*Metric) (resActive []byte, errActive error, resTrapper []byte, errTrapper error) {
+	var trapperMetrics []*Metric
+	var activeMetrics []*Metric
 
-	type DialResp struct {
-		Conn  *net.TCPConn
-		Error error
-	}
-
-	// Open connection to zabbix host
-	iaddr, err := s.getTCPAddr()
-	if err != nil {
-		return
-	}
-
-	// dial tcp and handle timeouts
-	ch := make(chan DialResp)
-
-	go func() {
-		conn, err = net.DialTCP("tcp", nil, iaddr)
-		ch <- DialResp{Conn: conn, Error: err}
-	}()
-
-	select {
-	case <-time.After(5 * time.Second):
-		err = fmt.Errorf("Connection Timeout")
-	case resp := <-ch:
-		if resp.Error != nil {
-			err = resp.Error
-			break
+	for i := 0; i < len(metrics); i++ {
+		if metrics[i].Active {
+			activeMetrics = append(activeMetrics, metrics[i])
+		} else {
+			trapperMetrics = append(trapperMetrics, metrics[i])
 		}
-
-		conn = resp.Conn
 	}
 
-	return
-}
-
-// Method Sender class, read data from connection.
-func (s *Sender) read(conn *net.TCPConn) (res []byte, err error) {
-	res = make([]byte, 1024)
-	res, err = ioutil.ReadAll(conn)
-	if err != nil {
-		err = fmt.Errorf("Error whule receiving the data: %s", err.Error())
-		return
+	if len(trapperMetrics) > 0 {
+		packetTrapper := NewPacket(trapperMetrics, false)
+		resTrapper, errTrapper = s.Send(packetTrapper)
 	}
 
-	return
+	if len(activeMetrics) > 0 {
+		packetActive := NewPacket(activeMetrics, true)
+		resActive, errActive = s.Send(packetActive)
+	}
+
+	return resActive, errActive, resTrapper, errTrapper
 }
 
-// Method Sender class, send packet to zabbix.
+// Send connects to Zabbix, send the data, return the response and close the connection
 func (s *Sender) Send(packet *Packet) (res []byte, err error) {
-	conn, err := s.connect()
+	// Timeout to resolve and connect to the server
+	conn, err := net.DialTimeout("tcp", s.Host+":"+strconv.Itoa(s.Port), s.ConnectTimeout)
 	if err != nil {
-		return
+		return res, fmt.Errorf("connecting to server (timeout=%v): %v", s.ConnectTimeout, err)
 	}
 	defer conn.Close()
 
 	dataPacket, _ := json.Marshal(packet)
 
-	/*
-	   fmt.Printf("HEADER: % x (%s)\n", s.getHeader(), s.getHeader())
-	   fmt.Printf("DATALEN: % x, %d byte\n", packet.DataLen(), len(packet.DataLen()))
-	   fmt.Printf("BODY: %s\n", string(dataPacket))
-	*/
-
 	// Fill buffer
 	buffer := append(s.getHeader(), packet.DataLen()...)
 	buffer = append(buffer, dataPacket...)
 
-	// Sent packet to zabbix
+	// Write timeout
+	conn.SetWriteDeadline(time.Now().Add(s.WriteTimeout))
+
+	// Send packet to zabbix
 	_, err = conn.Write(buffer)
 	if err != nil {
-		err = fmt.Errorf("Error while sending the data: %s", err.Error())
-		return
+		return res, fmt.Errorf("sending the data (timeout=%v): %s", s.WriteTimeout, err.Error())
 	}
 
-	res, err = s.read(conn)
+	// Read timeout
+	conn.SetReadDeadline(time.Now().Add(s.ReadTimeout))
 
-	/*
-	   fmt.Printf("RESPONSE: %s\n", string(res))
-	*/
-	return
+	// Read response from server
+	res, err = s.read(conn)
+	if err != nil {
+		return res, fmt.Errorf("reading the response (timeout=%v): %s", s.ReadTimeout, err.Error())
+	}
+
+	return res, nil
+}
+
+// RegisterHost provides a register a Zabbix's host with Autoregister method.
+func (s *Sender) RegisterHost(host, hostmetadata string) error {
+
+	p := &Packet{Request: "active checks", Host: host, HostMetadata: hostmetadata}
+
+	res, err := s.Send(p)
+	if err != nil {
+		return fmt.Errorf("sending packet: %v", err)
+	}
+
+	header := res[:4]
+	//length := res[4:13]
+	data := res[13:]
+
+	if bytes.Equal(header, s.getHeader()) {
+		return fmt.Errorf("response header is not valid")
+	}
+
+	response := Response{}
+	if err := json.Unmarshal(data, &response); err != nil {
+		fmt.Errorf("zabbix response is not valid: %v", err)
+	}
+
+	if response.Response == "success" {
+		return nil
+	}
+
+	// The autoregister process always return fail the first time
+	// We retry the process to get success response to verify the host registration properly
+	p = &Packet{Request: "active checks", Host: host, HostMetadata: hostmetadata}
+
+	res, err = s.Send(p)
+	if err != nil {
+		return fmt.Errorf("sending packet: %v", err)
+	}
+
+	header = res[:4]
+	//length := res[4:13]
+	data = res[13:]
+
+	if bytes.Equal(header, s.getHeader()) {
+		return fmt.Errorf("response header is not valid")
+	}
+
+	response = Response{}
+	if err = json.Unmarshal(data, &response); err != nil {
+		fmt.Errorf("zabbix response is not valid: %v", err)
+	}
+
+	if response.Response == "failed" {
+		return fmt.Errorf("autoregistration failed, verify hostmetadata")
+	}
+
+	return nil
 }
